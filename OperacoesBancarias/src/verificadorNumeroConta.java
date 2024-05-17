6ec1009b4206ddf3f2c878c0dcaf7ccd98cb56bb
LeviJJesus <levijardimdejesus5@gmail.com>
2024-05-17 16:38:41 -0300

    Resolvendo execício para verificar número da conta

--- OperacoesBancarias/src/verificadorNumeroConta.java
+++ OperacoesBancarias/src/verificadorNumeroConta.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class verificadorNumeroConta {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        try {
+            // System.out.println("Digite o número da conta bancária: ");
+            String numeroConta = scanner.nextLine();
+            // System.out.println(numeroConta.length());
+            verificarNumeroConta(numeroConta);
+            System.out.println("Numero de conta valido.");
+
+            
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: Numero de conta invalido. Digite exatamente 8 digitos.");
+        }
+    }
+    private static void verificarNumeroConta(String numeroConta) throws IllegalArgumentException{
+        if(numeroConta.length() != 8) {
+            throw new IllegalArgumentException();
+        }
     }
 }
