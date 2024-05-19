ec07b163397d316393aac73ef7a8114b7ad4aa77
LeviJJesus <levijardimdejesus5@gmail.com>
2024-05-19 16:30:51 -0300

    Resolvendo erro do exercício de controle de saque

--- OperacoesBancarias/src/ControleSimplesDeSaque.java
+++ OperacoesBancarias/src/ControleSimplesDeSaque.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 
 public class ControleSimplesDeSaque {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double limiteDiario = scanner.nextDouble(); // 3000
-        // double saque = 0;
+        double limiteDiario = scanner.nextDouble(); // Limite de saque diário
 
+        while (true) { // O loop continua até o usuário digitar 0 ou atingir o limite
+            double saque = scanner.nextDouble(); // Valor do saque
 
-        for(int i = 0; i <= 10; i++) {
-            double saque = scanner.nextDouble(); // 2000
             if (saque == 0) {
                 System.out.println("Transacoes encerradas.");
                 break;
             } else if (saque > limiteDiario) {
                 System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break;
             } else {
-                double valorNovo = limiteDiario - saque;
-                System.out.println("Saque realizado. Limite restante: " + valorNovo);
+                limiteDiario -= saque;
+                System.out.println("Saque realizado. Limite restante: " + limiteDiario);
+
+                if (limiteDiario == 0) {
+                    System.out.println("Limite diario atingido.");
+                    break;
+                }
             }
         }
     }
