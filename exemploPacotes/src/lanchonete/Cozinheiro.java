c8045fbbebc3b87e2edb580cc000a1a26f548b77
LeviJJesus <levijardimdejesus5@gmail.com>
2024-05-26 15:48:40 -0300

    Criando a primeira classe com o conceito de POO

---
+++ exemploPacotes/src/lanchonete/Cozinheiro.java
@@ -0,0 +1,7 @@
+package lanchonete;
+
+public class Cozinheiro {
+	public void adicionarLancheNoBalcao() {
+		System.out.println("Adicionando lanche natural hamburguer no balcão");
+	}
+}
--- novaEscola/src/module-info.java
+++ novaEscola/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module exemploPacotes {
+module novaEscola {
 }
